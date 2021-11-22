c8117113262283dfeb2b7a913c62107c194706cf
JD <38892748+TemporalCoder@users.noreply.github.com>
2021-11-22 13:03:56 +0000

    Add files via upload

---
+++ MapMaker/MapMaker/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace MapMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("OCR A Extended", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(187, 169);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(411, 451);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 68);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 73);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generate Rooms";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(627, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(425, 374);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(16, 465);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 63);
+            this.textBox2.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(181, 55);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(182, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 20;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(396, 55);
+            this.trackBar2.Maximum = 15;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(142, 56);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Value = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 149);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 39);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "S.L. Paths";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 260);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 39);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Do Walls";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.Location = new System.Drawing.Point(627, 396);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(425, 376);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 196);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 39);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "M.H. Paths";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "MapSize";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(393, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Number of Rooms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 784);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Map Maker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MapMaker/MapMaker/Form1.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapMaker
+{
+    public partial class Form1 : Form
+    {
+        int mapSize = 30;
+        int numberOfRooms = 5;
+
+        char[,] map;        
+        Vector2[] points; //class declared at bottom of this file! 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            map = new char[mapSize, mapSize];
+            points = new Vector2[numberOfRooms];
+
+            initMap();
+            generateRoom();
+            displayMap();
+        }
+        void initMap()
+        {   
+            mapSize = trackBar1.Value;
+            numberOfRooms = trackBar2.Value;
+
+            map = new char[mapSize, mapSize];
+            points = new Vector2[numberOfRooms];
+
+
+            for (int row = 0; row < mapSize; row++)
+            {
+                for (int col = 0; col < mapSize; col++)
+                {
+                    map[row, col] = '~';
+                }
+            }
+            displayMap();
+        }
+        void displayMap()
+        {
+            textBox1.Clear();
+            string buffer = "";
+            for (int row = 0; row < mapSize; row++)
+            {
+                for (int col = 0; col < mapSize; col++)
+                {
+                    buffer+=(map[row, col].ToString());
+
+                }
+                buffer+=("\r\n");
+            }
+            textBox1.Text = buffer;
+            Refresh();
+        }
+
+        void generateRoom()
+        {
+            Random rnd = new Random();
+            int roomSize = rnd.Next(5, 8);
+            int roomRow = rnd.Next(0, mapSize-roomSize-1);
+            int roomCol = rnd.Next(0, mapSize - roomSize - 1);
+
+            for (int row = roomRow; row < roomRow + roomSize; row++)
+            {
+                for (int col = roomCol; col < roomCol + roomSize; col++)
+                {
+                    if (row==roomRow||row==roomRow+roomSize-1||col==roomCol||col==roomCol+roomSize-1) 
+                    {
+                        map[row, col] = 'W';
+                    }
+                    else
+                    {
+                        map[row, col] = ' ';
+                    }
+                }
+            }
+        }
+
+        void generateRoom2()
+        {
+            Random rnd = new Random();
+            
+            for (int i = 0; i < numberOfRooms; i++)
+            {
+                int roomSize = rnd.Next(3, 7);
+                int roomRow = rnd.Next(2, mapSize - roomSize-1);
+                int roomCol = rnd.Next(2, mapSize - roomSize-1);
+
+                points[i] = new Vector2();
+                points[i].y = roomCol + (roomSize / 2);
+                points[i].x = roomRow + (roomSize / 2);
+
+                for (int row = roomRow; row < roomRow + roomSize; row++)
+                {
+                    for (int col = roomCol; col < roomCol + roomSize; col++)
+                    {                       
+                       map[row,col] = ' ';                       
+                    }
+                }
+
+                map[points[i].x, points[i].y] = 'X';
+               
+            }
+
+
+            //add corridors
+
+            //DrawLine(1,1,trackBar1.Value,trackBar2.Value);
+
+
+            /*
+            for (int row = 1; row < 20 - 1; row++)
+            {
+                for (int col = 1; col < 20 - 1; col++)
+                {
+                    if (map[row, col] == 'W')
+                    {
+                        int door = rnd.Next(1, 10);
+                        if(door==1)
+                        {
+                            map[row, col] = 'D';
+                        }
+                    }
+                }
+            }
+            */
+            displayMap();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initMap();
+            displayMap();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            initMap();
+            generateRoom2();
+            displayMap();
+            
+        }
+
+        void bresenham(int x1, int y1, int x2, int y2)
+        {
+            int m_new = 2 * (y2 - y1);
+            int slope_error_new = m_new - (x2 - x1);
+            for (int x = x1, y = y1; x <= x2; x++)
+            {
+                textBox2.AppendText(x + ":" + y + " \r\n");
+                map[y, x] = 'C';
+                // Add slope to increment angle formed
+                slope_error_new += m_new;
+
+                // Slope error reached limit, time to
+                // increment y and update slope error.
+                if (slope_error_new >= 0)
+                {
+                    y++;
+                    slope_error_new -= 2 * (x2 - x1);
+                }
+            }
+        }
+
+        void plotLine(int x0, int y0, int x1, int y1)
+        {
+            int dx, dy, p, x, y;
+
+            dx = x1 - x0;
+            dy = y1 - y0;
+
+            x = x0;
+            y = y0;
+
+            p = 2 * dy - dx;
+
+            while (x < x1)
+            {
+
+                textBox2.AppendText(x + ":" + y + " \r\n");
+
+                if (p >= 0)
+                {
+                    map[y, x]='P';
+                    y = y + 1;
+                    p = p + 2 * dy - 2 * dx;
+                }
+                else
+                {
+                    map[y, x] = 'P';
+                    p = p + 2 * dy;
+                }
+                x = x + 1;
+            }
+        }
+
+        private void DrawLine(int x0, int y0, int x1, int y1)
+        {
+            int xInitial = x0, yInitial = y0, xFinal = x1- 0, yFinal = y1- 0;
+            int dx = xFinal - xInitial, dy = yFinal - yInitial, steps, k, xf, yf;
+            float xIncrement, yIncrement, x = xInitial, y = yInitial;
+
+            if (Math.Abs(dx) > Math.Abs(dy))
+                steps = Math.Abs(dx);
+            else
+                steps = Math.Abs(dy);
+
+            xIncrement = dx / (float)steps;
+            yIncrement = dy / (float)steps;
+
+            for (k = 0; k < steps; k++)
+            {
+                x += xIncrement;
+                xf = (int)x;
+                y += yIncrement;
+                yf = (int)y;
+
+                map[xf, yf]='P';
+            }
+        }
+
+        void manhattenPaths(int x0, int y0, int x1, int y1)
+        {
+            bool atGoal = false;
+            while (atGoal == false)
+            {
+                if (x0 < x1)
+                {
+                    x0++;
+                    map[x0, y0] = 'P';
+                }
+                if (x0 > x1)
+                {
+                    x0--;
+                    map[x0, y0] = 'P';
+                }
+
+                if (y0 < y1)
+                {
+                    y0++;
+                    map[x0, y0] = 'P';
+                }
+                if (y0 > y1)
+                {
+                    y0--;
+                    map[x0, y0] = 'P';
+                }
+
+                if (x0 == x1 && y0 == y1) {atGoal = true; }
+            }
+        }
+
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen blackPen = new Pen(Color.Black, 1);
+
+            SolidBrush greyBrush = new SolidBrush(Color.Gray);
+            SolidBrush brownBrush = new SolidBrush(Color.Brown);
+            SolidBrush greenBrush = new SolidBrush(Color.GreenYellow);
+            SolidBrush reddyBrush = new SolidBrush(Color.SaddleBrown);
+            SolidBrush blueBrush = new SolidBrush(Color.DarkSlateGray );
+
+
+            for (int row = 0; row < mapSize; row++)
+            {
+                for (int col = 0; col < mapSize; col++)
+                {
+                    if (map[row, col] == '~') { e.Graphics.FillRectangle(greenBrush, new Rectangle(col * 10, row * 10, 10,  10)); }
+                    if (map[row, col] == ' ') { e.Graphics.FillRectangle(greyBrush, new Rectangle(col * 10, row * 10, 10, 10)); }
+                    if (map[row, col] == 'W') { e.Graphics.FillRectangle(brownBrush, new Rectangle(col * 10, row * 10, 10, 10)); }
+                    if (map[row, col] == 'D') { e.Graphics.FillRectangle(reddyBrush, new Rectangle(col * 10, row * 10, 10, 10)); }
+                    if (map[row, col] == 'P') { e.Graphics.FillRectangle(blueBrush, new Rectangle(col * 10, row * 10, 10, 10)); }
+
+                    e.Graphics.DrawRectangle(blackPen, new Rectangle(col*10, row*10, 10, 10));
+                }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            initMap();
+            generateRoom2();
+            displayMap();
+            Refresh();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < numberOfRooms - 1; i++)
+            {
+                DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+                //manhattenPaths(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+            }
+            displayMap();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //add walls by checking neighbours
+            for (int row = 1; row < mapSize - 1; row++)
+            {
+                for (int col = 1; col < mapSize - 1; col++)
+                {
+                    if (map[row, col] == '~')
+                    {
+                        if (map[row - 1, col] == ' ') { map[row, col] = 'W'; }
+                        if (map[row + 1, col] == ' ') { map[row, col] = 'W'; }
+                        if (map[row, col - 1] == ' ') { map[row, col] = 'W'; }
+                        if (map[row, col + 1] == ' ') { map[row, col] = 'W'; }
+                    }
+                    if (map[row, col] == 'P')
+                    {
+                        if (map[row - 1, col] == '~') { map[row-1, col] = 'W'; }
+                        if (map[row + 1, col] == '~') { map[row+1, col] = 'W'; }
+                        if (map[row, col - 1] == '~') { map[row, col-1l] = 'W'; }
+                        if (map[row, col + 1] == '~') { map[row, col+1] = 'W'; }
+                    }
+                }
+            }
+            displayMap();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < numberOfRooms - 1; i++)
+            {
+                //DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+                manhattenPaths(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+            }
+            displayMap();
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            Pen blackPen = new Pen(Color.Black, 1);
+
+            SolidBrush greyBrush = new SolidBrush(Color.Gray);
+            SolidBrush lightBrush = new SolidBrush(Color.LightGray);
+            SolidBrush slateBrush = new SolidBrush(Color.DarkSlateBlue);
+
+
+            for (int row = 0; row < mapSize; row++)
+            {
+                for (int col = 0; col < mapSize; col++)
+                {
+                    char t = map[row, col];
+                    int tileSize = 10;
+
+                    switch(t)
+                    {
+                        case '~':
+                            e.Graphics.FillRectangle(greyBrush, new Rectangle(col * tileSize, row * tileSize, tileSize, tileSize));
+                            break;
+
+                        case ' ':
+                        case 'P':
+                            e.Graphics.FillRectangle(lightBrush, new Rectangle(col * tileSize, row * tileSize, tileSize, tileSize));
+                            break;
+
+                        case 'W':
+                            e.Graphics.FillRectangle(slateBrush, new Rectangle(col * tileSize, row * tileSize, tileSize, tileSize));
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    e.Graphics.DrawRectangle(blackPen, new Rectangle(col * 10, row * 10, 10, 10));
+                }
+            }
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = "MapSize: " + trackBar1.Value.ToString();
+            mapSize = trackBar1.Value;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = "Number of Rooms: " + trackBar2.Value.ToString();
+            numberOfRooms = trackBar2.Value;
+        }
+    }
+
+
+    class Vector2
+    {
+        public int x;
+        public int y;
+
+    }
+}
